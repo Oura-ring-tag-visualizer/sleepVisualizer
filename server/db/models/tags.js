e8ec9707b134944311699e81d569c17d6c43c40a
sohamdutta <xzoham@gmail.com>
2020-06-24 17:27:16 -0400

    feat: models set up

---
+++ server/db/models/tags.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Tags = db.define('tags', {
+  date: {
+    type: Sequelize.DATEONLY,
+    allowNull: false
+  },
+  tagName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+})
