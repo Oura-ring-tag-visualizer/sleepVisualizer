c55b5c9fe96e3f1b4a0288e1656d0d794edf4d69
Alex <albangiyev@gmail.com>
2020-06-25 16:40:38 -0400

    feat: api route for tagName

--- server/api/tags.js
+++ server/api/tags.js
@@ -1,19 +1,16 @@
 const router = require('express').Router()
-const {Tags, Rem} = require('../db/models')
+const {Tags} = require('../db/models')
+const {Op} = require('sequelize')
 module.exports = router
 
 router.get('/:tagName', async (req, res, next) => {
   try {
     const dates = await Tags.findAll({
       where: {
-        tagNames: req.params.tagName
-      },
-      include: [
-        {
-          model: Rem,
-          attributes: ['remSleepTime']
+        tagNames: {
+          [Op.contains]: [req.params.tagName]
         }
-      ]
+      }
     })
     console.log(dates)
     res.json(dates)
