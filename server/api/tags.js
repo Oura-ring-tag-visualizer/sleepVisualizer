de08aba5942f69d0c5d642dc47189e8fec418d6f
sohamdutta <xzoham@gmail.com>
2020-06-25 16:40:41 -0400

    can retrieve data

--- server/api/tags.js
+++ server/api/tags.js
@@ -1,19 +1,30 @@
 const router = require('express').Router()
-const {Tags, Rem} = require('../db/models')
+const {Tags} = require('../db/models')
 module.exports = router
+const {Op} = require('sequelize')
+
+// router.get('/:tagName', async (req, res, next) => {
+//   try {
+//     const data = await Tags.findAll({
+//       where : {
+//         tagNames
+//       }
+//     })
+
+//   } catch (err) {
+//     next(err)
+
+//   }
+// })
 
 router.get('/:tagName', async (req, res, next) => {
   try {
     const dates = await Tags.findAll({
       where: {
-        tagNames: req.params.tagName
-      },
-      include: [
-        {
-          model: Rem,
-          attributes: ['remSleepTime']
+        tagNames: {
+          [Op.contains]: [req.params.tagName]
         }
-      ]
+      }
     })
     console.log(dates)
     res.json(dates)
