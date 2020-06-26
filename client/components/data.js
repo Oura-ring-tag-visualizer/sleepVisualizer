7dad6b3573a762583ea2bf41ba36adb1b2f3d1f3
Alex <albangiyev@gmail.com>
2020-06-26 17:16:45 -0400

    feat: multiple lines with chart.j

--- client/components/data.js
+++ client/components/data.js
@@ -1,58 +1,81 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {fetchAllDates, fetchTag} from '../store/tags'
-import ResponsiveContainer from 'recharts/lib/component/ResponsiveContainer'
-import LineChart from 'recharts/lib/chart/LineChart'
-import Line from 'recharts/lib/cartesian/Line'
-import XAxis from 'recharts/lib/cartesian/XAxis'
-import YAxis from 'recharts/lib/cartesian/YAxis'
-import CartesianGrid from 'recharts/lib/cartesian/CartesianGrid'
-import Tooltip from 'recharts/lib/component/Tooltip'
-import Legend from 'recharts/lib/component/Legend'
+import {Line} from 'react-chartjs-2'
 
 class Data extends React.Component {
   componentDidMount = () => {
     this.props.fetchDates()
     this.props.fetchTagData('coffee')
   }
+
   render() {
-    const datesOnly = this.props.dates.map(date => {
+    const datesAndRem = this.props.dates.map(date => {
       delete date.tagNames
       delete date.createdAt
       delete date.updatedAt
       return date
     })
 
-    // const remSleepTimeAverage = this.props.tags.map(tag => {
-    //   delete date.tagNames
-    //   delete date.createdAt
-    //   delete date.updatedAt
-    //   return (
+    const datesOnly = datesAndRem.map(date => {
+      return date.date
+    })
 
-    //   )
-    // })
+    const remOnly = datesAndRem.map(date => {
+      return date.remSleepTime
+    })
 
-    console.log('dates: ', datesOnly)
-    console.log('tagNameData:', this.props.tags)
+    console.log('datesOnly: ', datesOnly)
+    console.log('remOnly: ', remOnly)
+
+    const chartData = {
+      labels: datesOnly,
+      datasets: [
+        {
+          label: ['Rem sleep time'],
+          data: remOnly,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.6)',
+            'rgba(54, 162, 235, 0.6)',
+            'rgba(255, 206, 86, 0.6)',
+            'rgba(75, 192, 192, 0.6)',
+            'rgba(153, 102, 255, 0.6)',
+            'rgba(255, 159, 64, 0.6)',
+            'rgba(255, 99, 132, 0.6)'
+          ]
+        },
+
+        {
+          label: ['Rem sleep'],
+          data: [1234, 3456, 2854, 9859, 1764],
+          backgroundColor: [
+            'rgba(75,192,192,1)',
+            'rgba(75,192,192,1)',
+            'rgba(75,192,192,1)',
+            'rgba(75,192,192,1)',
+            'rgba(75,192,192,1)',
+            'rgba(75,192,192,1)'
+          ]
+        }
+      ]
+    }
+
     return (
-      <div>
-        <div>
-          <ResponsiveContainer width="99%" height={500}>
-            <LineChart data={datesOnly}>
-              <XAxis dataKey="date" stroke="black" />
-              <YAxis stroke="black" dataKey="remSleepTime" />
-              <CartesianGrid vertical={false} strokeDasharray="3 3" />
-              <Tooltip />
-              <Legend />
-              <Line
-                type="monotone"
-                dataKey="remSleepTime"
-                stroke="black"
-                activeDot={{r: 8}}
-              />
-            </LineChart>
-          </ResponsiveContainer>
-        </div>
+      <div className="chart">
+        <Line
+          data={chartData}
+          options={{
+            title: {
+              display: this.props.displayTitle,
+              text: 'REM Sleep Time by Tag',
+              fontSize: 25
+            },
+            legend: {
+              display: this.props.displayLegend,
+              position: this.props.legendPosition
+            }
+          }}
+        />
       </div>
     )
   }
